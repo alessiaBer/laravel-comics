c83bf3ddcfa44c69eb715acf0ef5e6e5670040a2
alessiaBer <alexdeyn@gmail.com>
2023-05-23 22:21:20 +0200

    [fix] imgs

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @include('partials.jumbotron')
 
-<section class="current_series">
+<section class="current_series bg-dark">
     <div class="container my_container_lg">
         <span class="section_title bg-primary text-white">
             <h4 class="d-inline">CURRENT SERIES</h4>
@@ -12,7 +12,7 @@
             <div class="row row-col-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 mt-6">
                 @foreach($comics as $comic)
                 <div class="col">
-                    <div class="card">
+                    <div class="card rounded-0 border-0 bg-transparent">
                         <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
                         <div class="card_text">
                             <span class="text-uppercase text-white">{{$comic['title']}}</span>
